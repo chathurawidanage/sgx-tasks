c2aa820f5799c016f94d4cfa9e64e33f4f743a42
Chathura Widanage <chathurawidanage@gmail.com>
2020-12-07 13:14:44 -0500

    auto generating worker id

--- task.cpp
+++ task.cpp
@@ -4,10 +4,11 @@
 #include <thread>
 
 #include "spdlog/spdlog.h"
+#include "uuid.hpp"
 #include "worker.hpp"
 
 int main(int argc, char *argv[]) {
-    tasker::Worker worker(argv[1]);
+    tasker::Worker worker(gen_random(16));
     worker.OnMessage([&worker](std::string msg) {
         spdlog::info("Message received from server : {}", msg);
         std::string cmd = tasker::GetCommand(tasker::Commands::MESSAGE);
@@ -20,7 +21,7 @@ int main(int argc, char *argv[]) {
     });
     std::string server_url = "tcp://localhost:5050";
     if (argc == 3) {
-        server_url = argv[2];
+        server_url = argv[1];
     }
     worker.Start(server_url);
     return 0;
