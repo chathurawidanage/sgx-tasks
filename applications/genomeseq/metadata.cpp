de4088f4ee673a1ea3c9e5b75faa8360b4285565
Chathura Widanage <chathurawidanage@gmail.com>
2021-03-01 16:54:16 -0500

    fixed metadata write

--- applications/genomeseq/metadata.cpp
+++ applications/genomeseq/metadata.cpp
@@ -2,10 +2,12 @@
 
 #include <filesystem>
 #include <fstream>
+#include <spdlog/spdlog.h>
 
 #include "commands.hpp"
 
 void Metadata::Read() {
+    spdlog::info("Reading metadata from {}", GetFile());
     std::ifstream infile(GetFile());
     infile >> id;
     infile >> source;
@@ -23,14 +25,14 @@ bool Metadata::Exists() {
 
 void Metadata::Write() {
     std::ofstream outfile(GetFile());
-    outfile << id;
-    outfile << source;
-    outfile << partitions;
+    outfile << id << std::endl;
+    outfile << source << std::endl;
+    outfile << partitions << std::endl;
     outfile.close();
 }
 
 std::shared_ptr<Metadata> Metadata::Load(std::string id) {
-    auto m = std::shared_ptr<Metadata>{new Metadata(id)};
+    auto m = std::make_shared<Metadata>(id);
     m->Read();
     return m;
 }
